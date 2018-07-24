41e8078765d4be1fb1417e20a9ba34d5998fa463
Thiago Queiroz <bob@rogalabs.com>
2018-07-24 15:27:42 -0300

    Create devfest layout component

---
+++ src/app/layouts/devfest/devfest.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-devfest',
+  templateUrl: './devfest.component.html',
+  styleUrls: ['./devfest.component.scss']
+})
+export class DevfestLayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
