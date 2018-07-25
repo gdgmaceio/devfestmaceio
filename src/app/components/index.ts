596f586c4133702fbe9e6d5bb7e0f7df78472325
Thiago Queiroz <bob@rogalabs.com>
2018-07-25 18:15:34 -0300

    Create we are back component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,4 @@
 export * from './header';
 export * from './logo';
 export * from './navigation';
+export * from './weareback';
---
+++ src/app/components/weareback/weareback.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-we-are-back',
+  templateUrl: './weareback.component.html',
+  styleUrls: ['./weareback.component.scss']
+})
+
+export class WeAreBackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
