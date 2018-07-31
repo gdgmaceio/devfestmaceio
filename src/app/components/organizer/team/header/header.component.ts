bd2fe5df5e8ba2452a819ee5a059df80ec03e4f1
Thiago Queiroz <bob@rogalabs.com>
2018-07-31 17:38:57 -0300

    Create team component

---
+++ src/app/components/organizer/team/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class TeamHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
