178e6d8f00e0885b8cdb2d2204957c901de112a4
Thiago Queiroz <bob@rogalabs.com>
2018-08-12 14:14:27 -0300

    Add firebase config environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2agZi5ND1DBZr7bP_nrbEwG8WzD2tRoc',
+    authDomain: 'devfestmaceio.firebaseapp.com',
+    databaseURL: 'https://devfestmaceio.firebaseio.com"',
+    projectId: 'devfestmaceio',
+    storageBucket: 'devfestmaceio.appspot.com',
+    messagingSenderId: '62495706834'
+  },
 };
