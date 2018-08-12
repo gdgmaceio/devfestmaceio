a5b93a6b2464d917b217bdaaf1ab3b0c5fda67f1
Thiago Queiroz <bob@rogalabs.com>
2018-08-12 14:12:22 -0300

    AngularFirestoreModule and CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
 // Import layouts
@@ -10,7 +11,12 @@ import { DevfestLayoutComponent } from './layouts';
 const APP_LAYOUTS = [DevfestLayoutComponent];
 
 // Import components
-import { AppHeaderComponent, AppLogoComponent, AppNavigationComponent, AppFooterComponent } from './components';
+import {
+  AppHeaderComponent,
+  AppLogoComponent,
+  AppNavigationComponent,
+  AppFooterComponent
+} from './components';
 
 const APP_COMPONENTS = [
   AppHeaderComponent,
@@ -22,16 +28,17 @@ const APP_COMPONENTS = [
 // Import routing module
 import { AppRoutingModule } from './app.routing';
 
+// Import core module
+import { CoreModule } from './core';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ...APP_LAYOUTS,
-    ...APP_COMPONENTS
-  ],
+  declarations: [AppComponent, ...APP_LAYOUTS, ...APP_COMPONENTS],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
