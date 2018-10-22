a5547e3a677e55abea05452f99eb0c194b9d59fe
Thiago Queiroz <eaebob@gmail.com>
2018-10-22 03:53:00 -0300

    Import coremodule

--- src/app/components/schedule/schedule.module.ts
+++ src/app/components/schedule/schedule.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CoreModule } from './../../core';
 import { ScheduleComponent } from './schedule.component';
 import { ScheduleListComponent } from './list/list.component';
 import { ScheduleHeaderComponent } from './header/header.component';
 import { ScheduleService } from './../../services/schedule.service';
 
 @NgModule({
+  imports: [CoreModule],
   declarations: [
     ScheduleComponent,
     ScheduleHeaderComponent,
